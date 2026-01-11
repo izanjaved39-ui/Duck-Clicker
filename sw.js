d420e01ed996d9fb70646cd08b0b67d725748153
izanjaved39-ui <izanjaved39@gmail.com>
2026-01-11 17:09:49 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,5 @@
-const CACHE_NAME = "duckclicker-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
 });
